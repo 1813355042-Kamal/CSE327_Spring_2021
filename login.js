97c984ed362e11e0279b0305de8a8b62e23b9fe0
Salman Sahel <salman.exe@gmail.com>
2021-05-04 23:35:37 +0600

    Implement Login Persistence

--- login.js
+++ login.js
@@ -23,6 +23,31 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+function persistentlogin(){
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(() => {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+
+        var userEmail = document.getElementById("email_field").value;
+        var userPass = document.getElementById("password_field").value;
+
+        return firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function(usr_){
+            window.location.href = 'map.html';
+        }).catch(function(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            window.alert("Error : " + errorMessage);
+        });
+    }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("Unexpected error in persistent login");
+    });
+}
 function login(){
 
   var userEmail = document.getElementById("email_field").value;
@@ -38,6 +63,13 @@ function login(){
     // ...
   });
 
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function(usr_){
+        window.location.href = 'map.html';
+    }).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("Error : " + errorMessage);
+    });
 }
 
 function logout(){
